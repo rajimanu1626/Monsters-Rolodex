9adfde5fb49c00c417396af5ff5dc552c51392fc
rajimanu1626 <rajimanu1626@gmail.com>
2020-09-01 12:14:28 +0530

    Updates

---
+++ precache-manifest.d77fef477f631286303d9eaed47883c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9818318031a8fcf49d3f90b5579537c3",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7edcfff4cd71eda41fe7",
+    "url": "/Monsters-rolodex/static/css/main.6377d205.chunk.css"
+  },
+  {
+    "revision": "8e91849f3a551595a643",
+    "url": "/Monsters-rolodex/static/js/2.3d07971f.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/Monsters-rolodex/static/js/2.3d07971f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7edcfff4cd71eda41fe7",
+    "url": "/Monsters-rolodex/static/js/main.df3c8faf.chunk.js"
+  },
+  {
+    "revision": "e84a563926e204ec1909",
+    "url": "/Monsters-rolodex/static/js/runtime-main.1bdb9093.js"
+  }
+]);
